3ee23a6a2e7099435a78513a55bfa63361fe4fdc
adil1214 <adil1350@gmail.com>
2018-04-30 14:21:29 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', '/public');
+
+let app = express();
+
+app.use(express.static(publicPath));
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server started on ${port}` );
+});
